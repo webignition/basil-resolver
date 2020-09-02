c4ea890a984b5b7a67546ba035a9f1ebe51df062
Jon Cram <webignition@gmail.com>
2020-09-02 17:46:47 +0100

    Revert "Support multiple browsers per test (#47)"
    This reverts commit dddc413adbc345ec772c3dd5e067fcbd2a189abc.

--- src/TestConfigurationResolver.php
+++ src/TestConfigurationResolver.php
@@ -41,6 +41,6 @@ class TestConfigurationResolver
             }
         }
 
-        return new Configuration($configuration->getBrowsers(), $url);
+        return new Configuration($configuration->getBrowser(), $url);
     }
 }
